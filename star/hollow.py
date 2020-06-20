6c2940988a1047b9de975d2631af349a40e55a6f
Paupar <64657169+g-arkantos@users.noreply.github.com>
2020-06-20 21:30:10 +0530

    Update hollow.py

--- star/hollow.py
+++ star/hollow.py
@@ -17,30 +17,6 @@ def square(row):
 		print("*", end=' ')
 	print()	
 
-def triangle(base):
-	if base%2==0:
-		d=base//2
-		w=1
-		print()
-		for i in range(1,d):
-			if i==1:
-				print('*')
-			for star in range(1):
-				print('*', end=' ')
-			for space in range(w):
-				print(" ", end=' ')
-				w=w+1
-			for star in range(1):
-				print('*', end=' ')
-		
-		for star in range(d):
-			print("*")
-		print()		
-
-
-	
-
 
 x=int(input("Enter rows: "))
 square(x)
-triangle(x)
